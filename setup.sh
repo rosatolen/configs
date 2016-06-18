c618ed6f63a17c598055958f0a149656d253f03c
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-06-18 17:57:36 -0500

    Changes

--- setup.sh
+++ setup.sh
@@ -10,16 +10,14 @@ if [[ $1 == 'reset' ]]; then
     exit
 fi
 
-_sudo () {
-    echo "Running: '$@' in `pwd`"
-    sudo $@
-}
+_sudo () { echo "Running: '$@' in `pwd`"; sudo $@; }
+
+isArch() { return $([[ `uname -r` == *"ARCH"* ]]); }
 
 pkgmgr_install () {
     if [ -f /etc/fedora-release ]; then
         _sudo dnf install $@
-    fi
-    if [[ `uname -r` == *"ARCH"* ]]; then
+    elif isArch; then
         _sudo pacman -S $@
     fi
 }
@@ -30,10 +28,9 @@ dev_tools() {
 
 direnv_setup() {
     VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/direnv/direnv/releases/latest | cut -d / -f8)
-    wget https://github.com/direnv/direnv/releases/download/$VERSION/direnv.linux-amd64 -O ~/Downloads/direnv.linux-amd64
-    mv ~/Downloads/direnv.linux-amd64 ~/Downloads/direnv
-    chmod u+x ~/Downloads/direnv
-    _sudo mv ~/Downloads/direnv /usr/bin/
+    wget https://github.com/direnv/direnv/releases/download/$VERSION/direnv.linux-amd64 -O $HOME/direnv.linux-amd64
+    chmod u+x $HOME/direnv.linux-amd64
+    _sudo mv $HOME/direnv.linux-amd64 /usr/bin/direnv
 }
 
 git_setup () {
@@ -53,36 +50,17 @@ vim_setup () {
     rm spf13-vim.sh
 }
 
-virtualbox_part1 () {
-    wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo
-    dnf update
-    if [ $(rpm -qa kernel | sort -V | tail -n 1) != kernel-$(uname-r) ]; then
-        echo "Kernel versions do not match"
-        exit 1
-    else
-        echo "IMPORTANT!!"
-        echo "Reboot before continuing"
-    fi
-}
-
-virtualbox_part2 () {
-    if [ -z "$1" ]; then
-        echo "Error: No username supplied for VirtualBox setup"
-        echo "Usage: $0 <username>"
-        exit
-    fi
-    pkgmgr_install binutils gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
-    pkgmgr_install VirtualBox-5.0
-    _sudo /usr/lib/virtualbox/vboxdrv.sh setup
-    usermod -a -G vboxusers $1
-    VirtualBox
-}
-
 go_setup () {
-    pkgmgr_install go
-    mkdir .gopath   # Global Gopath
-    mkdir .gopaths  # Project Specific Gopath
-    cp envrc.example .gopaths
+    if ! hash go 2>/dev/null; then
+        pkgmgr_install go
+    fi
+    if [ ! -d $HOME/.gopath; ]; then
+        mkdir $HOME/.gopath   # Global Gopath
+    fi
+    if [ ! -d $HOME/.gopaths; ]; then
+        mkdir $HOME/.gopaths  # Project Specific Gopath
+    fi
+    cp ./env.example $HOME/.gopaths
 }
 
 yubikey_setup() {
@@ -103,82 +81,6 @@ yubikey_setup() {
     _sudo make install
     cd ..
     rm yubikey-personalization
-}
-
-tor_setup() {
-    pkgmgr_install tor
-    pkgmgr_install parcimonie.sh
-}
-
-gnupg_setup() {
-    dev_tools
-    tor_setup
-
-    mkdir $gnupg_src_dir && cd $gnupg_src_dir
-
-    wget https://gnupg.org/signature_key.html
-    begin_sig_line_num=$(cat signature_key.html | grep -ne '\-\-\-\-\-BEGIN.*BLOCK\-\-\-\-\-' | sed 's/^\([0-9]\+\):.*$/\1/')
-    end_sig_line_num=$(cat signature_key.html | grep -ne '\-\-\-\-\-END.*BLOCK\-\-\-\-\-' | sed 's/^\([0-9]\+\):.*$/\1/')
-    sed -n "$begin_sig_line_num"','"$end_sig_line_num"'p' signature_key.html | gpg --import
-
-    wget https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2
-    wget https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2.sig
-    gpg --verify libgpg-error-1.22.tar.bz2.sig
-    tar xjvf libgpg-error-1.22.tar.bz2
-    cd libgpg-error-1.22
-    ./autogen.sh
-    ./configure --enable-maintainer-mode && make
-    _sudo make install
-    cd ..
-
-    wget https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2
-    wget https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2.sig
-    gpg --verify libgcrypt-1.7.0.tar.bz2.sig
-    tar xjvf libgcrypt-1.7.0.tar.bz2
-    cd libgcrypt-1.7.0
-    ./autogen.sh
-    ./configure --enable-maintainer-mode && make
-    _sudo make install
-    cd ..
-
-    wget https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2
-    wget https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2.sig
-    gpg --verify libassuan-2.4.2.tar.bz2.sig
-    tar xjvf libassuan-2.4.2.tar.bz2
-    cd libassuan-2.4.2
-    ./autogen.sh
-    ./configure --enable-maintainer-mode && make
-    _sudo make install
-    cd ..
-
-    wget https://gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2
-    wget https://gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2.sig
-    gpg --verify libksba-1.3.4.tar.bz2.sig
-    tar xjvf libksba-1.3.4.tar.bz2
-    cd libksba-1.3.4
-    ./autogen.sh
-    ./configure --enable-maintainer-mode && make
-    _sudo make install
-    cd ..
-
-    wget https://gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2
-    wget https://gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2.sig
-    gpg --verify npth-1.2.tar.bz2.sig
-    tar xjvf npth-1.2.tar.bz2
-    cd npth-1.2
-    ./autogen.sh
-    ./configure --enable-maintainer-mode && make
-    _sudo make install
-    cd ..
-
-    pkgmgr_install gnupg2
-}
-
-coyim_setup () {
-    tor_setup
-    wget https://dl.bintray.com/twstrike/coyim/v0.3.5/linux/amd64/coyim -O ~/Downloads/coyim
-    chmod u+x ~/Downloads/coyim
-    mv ~/Downloads/coyim /usr/bin
 }
 
 gitcrypt_setup() {
@@ -219,4 +121,5 @@ basic_install() {
     gitcrypt_setup
     vim_setup
     ssh_pub_setup $1
+    direnv_setup
 }
